6e84cad8ae0abd1cb1fac6956ebc7c80791389c8
Max Tishenko <rd-max@ya.ru>
2023-04-02 15:16:23 +0300

    fibo done

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,13 +1,22 @@
 import { useState, ChangeEvent } from 'react';
 
-// input, onChange, setInput
 export function useSimpleForm(inputValue: string) {
   const [input, setInput] = useState(inputValue);
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    // event.preventDefault()
     const input = event.target.value;
+    console.log(input);
     setInput(input);
   };
   return { input, onChange, setInput };
+}
+
+export function useForm(inputValues: any) {
+  const [values, setValues] = useState(inputValues);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+  return { values, onChange, setValues };
 }
